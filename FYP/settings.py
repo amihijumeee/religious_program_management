f83199d4df180b87557c64fe99d7b7fe820db574
amihijumeee <syahmi3003@gmail.com>
2026-06-23 23:34:41 +0800

    update

--- FYP/settings.py
+++ FYP/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&&l79a#6@ph4&v4fr=s_soi8b4w6mg43bh^$s*&wb73mw2asu0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1",".onrender.com"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Default primary key field type
